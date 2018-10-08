4469cd0d8749df4283cd351625260cce285a08db
bojantomic <tomicb@fon.rs>
2018-10-08 08:07:21 +0200

    svi mateirijali ubaceni

---
+++ dvocas1/src/zadatak1/ProbaTelevizor.java
@@ -0,0 +1,22 @@
+package zadatak1;
+
+class ProbaTelevizor {
+
+	public static void main(String[] args) {
+		Televizor t = new Televizor();
+		int trenutna_jacina_tona;
+
+		t.ispisi();
+		t.pojacajTon();
+		t.ispisi();
+		t.smanjiTon();
+		t.ispisi();
+		t.promeniProgram(5);
+		t.ispisi();
+
+		trenutna_jacina_tona = t.vratiJacinuTona();
+
+		System.out.println("Trenutno, jacina tona na televizoru iznosi " + trenutna_jacina_tona);
+	}
+
+}
